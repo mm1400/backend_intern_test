8ed360f82298bfa9d36fe44692e9668b46580ed6
mm1400 <marin.melentii@gmail.com>
2025-02-17 18:08:44 -0500

    Add test for invalid id in query.test.ts

--- src/test/query.test.ts
+++ src/test/query.test.ts
@@ -2,11 +2,15 @@ import * as chai from 'chai';
 const expect = chai.expect;
 import *  as sinon from  'sinon';
 import { prisma } from '../graphql/prisma';
-import { Query } from '../graphql/query';
 import { Context } from '../graphql/context';
 import { graphql } from 'graphql';
 import { schema } from '../graphql/schema';
 import { Todo } from '@prisma/client';
+
+
+// https://github.com/prisma/prisma/discussions/18734
+// https://daily.dev/blog/graphql-testing-for-beginners#schema-tests
+
 
 const mockTodos: Todo[] = [
   { id: '1', title: 'Test Todo 1', completed: false, createdAt: new Date(), updatedAt: new Date(), dueDate: new Date() },
@@ -14,6 +18,10 @@ const mockTodos: Todo[] = [
 ];
 
 describe('Todo tests', () => {
+
+  afterEach(function () {
+    sinon.restore();
+  });
 
   it('should return correct todo for an id', async () => {
 
@@ -48,4 +56,32 @@ describe('Todo tests', () => {
     }
     expect(result?.data?.todo).to.deep.equal(expectedTodo);
   });
-})
+
+  it("should return null if id doesn't exist", async () => {
+    sinon.replace(prisma.todo, 'findUnique', sinon.fake.resolves(null));
+    const query = `
+      query GetTodo($id: ID!) {
+        todo(id: $id) {
+          id
+          title
+          completed
+          createdAt
+          updatedAt
+          dueDate
+        }
+      }
+    `
+    const variables = { id : '999999999'};
+    const context: Context = { prisma };
+
+    const result = await graphql({
+      schema: schema,
+      source: query,
+      variableValues: variables,
+      contextValue: context
+    });
+
+    expect(result?.data?.todo).to.be.null;
+    console.log(result);
+  });
+});
